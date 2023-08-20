c1b368a1b0a2645668bd7e325b4da202e249f9d3
Alfredo <agore@ranker.com>
2023-08-20 19:20:46 -0300

    Removed absolute path

--- public/js/app.js
+++ public/js/app.js
@@ -8,19 +8,17 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-          console.log(data.location);
-          console.log(data.forecast);
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+  });
   console.log("TESTING");
 });
